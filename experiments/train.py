9d561f32993c9542d9a4c93b7269dcb8d35e8cda
Siddharth <sid@ava.ri.cmu.edu>
2020-05-30 02:35:51 -0400

    changes for partial training

--- experiments/train.py
+++ experiments/train.py
@@ -3,10 +3,11 @@ import numpy as np
 import tensorflow as tf
 import time
 import pickle
-
+import matplotlib.pyplot as plt
 import maddpg.common.tf_util as U
 from maddpg.trainer.maddpg import MADDPGAgentTrainer
 import tensorflow.contrib.layers as layers
+
 
 def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
@@ -24,17 +25,28 @@ def parse_args():
     parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
     # Checkpointing
     parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
-    parser.add_argument("--save-dir", type=str, default="/tmp/policy/", help="directory in which training state and model should be saved")
-    parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="", help="directory in which training state and model are loaded")
+    parser.add_argument("--save-dir", type=str, default="/serverdata/sid/particle_env_policies/",
+                        help="directory in which training state and model should be saved")
+    parser.add_argument("--save-rate", type=int, default=1000,
+                        help="save model once every time this many episodes are completed")
+    parser.add_argument("--load-dir", type=str, default="",
+                        help="directory in which training state and model are loaded")
     # Evaluation
     parser.add_argument("--restore", action="store_true", default=False)
+    parser.add_argument("--train_adversaries", action="store_true", default=False)
+    parser.add_argument("--train_positive_agent", action="store_true", default=True)
+
+    # parser.add_argument("--", action="store_true", default=False)
     parser.add_argument("--display", action="store_true", default=False)
     parser.add_argument("--benchmark", action="store_true", default=False)
     parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
-    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/", help="directory where benchmark data is saved")
-    parser.add_argument("--plots-dir", type=str, default="./learning_curves/", help="directory where plot data is saved")
+    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/",
+                        help="directory where benchmark data is saved")
+    parser.add_argument("--plots-dir", type=str, default="./learning_curves/",
+                        help="directory where plot data is saved")
+
     return parser.parse_args()
+
 
 def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=None):
     # This model takes as input an observation and returns values of all actions
@@ -44,6 +56,7 @@ def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=Non
         out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.relu)
         out = layers.fully_connected(out, num_outputs=num_outputs, activation_fn=None)
         return out
+
 
 def make_env(scenario_name, arglist, benchmark=False):
     from multiagent.environment import MultiAgentEnv
@@ -60,6 +73,7 @@ def make_env(scenario_name, arglist, benchmark=False):
         env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
     return env
 
+
 def get_trainers(env, num_adversaries, obs_shape_n, arglist):
     trainers = []
     model = mlp_model
@@ -67,11 +81,11 @@ def get_trainers(env, num_adversaries, obs_shape_n, arglist):
     for i in range(num_adversaries):
         trainers.append(trainer(
             "agent_%d" % i, model, obs_shape_n, env.action_space, i, arglist,
-            local_q_func=(arglist.adv_policy=='ddpg')))
+            local_q_func=(arglist.adv_policy == 'ddpg')))
     for i in range(num_adversaries, env.n):
         trainers.append(trainer(
             "agent_%d" % i, model, obs_shape_n, env.action_space, i, arglist,
-            local_q_func=(arglist.good_policy=='ddpg')))
+            local_q_func=(arglist.good_policy == 'ddpg')))
     return trainers
 
 
@@ -79,6 +93,7 @@ def train(arglist):
     with U.single_threaded_session():
         # Create environment
         env = make_env(arglist.scenario, arglist, arglist.benchmark)
+
         # Create agent trainers
         obs_shape_n = [env.observation_space[i].shape for i in range(env.n)]
         num_adversaries = min(env.n, arglist.num_adversaries)
@@ -93,12 +108,38 @@ def train(arglist):
             arglist.load_dir = arglist.save_dir
         if arglist.display or arglist.restore or arglist.benchmark:
             print('Loading previous state...')
-            U.load_state(arglist.load_dir)
+            print("path is: ", arglist.load_dir)
+
+            # added for selective training.
+            # Make it general for other environments as well later.
+            if arglist.scenario == "simple_tag":
+                if not arglist.train_adversaries:
+                    saver = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                                                      scope="agent_" + str(num_adversaries)))
+                if not arglist.train_positive_agent:
+                    var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="agent_0")
+                    for l in range(1, arglist.num_adversaries):
+                        var_list += tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="agent_" + str(l))
+
+                    saver = tf.train.Saver(var_list=var_list)
+
+                U.load_state(arglist.load_dir, saver=saver)
+
+            else:
+                U.load_state(arglist.load_dir)
 
         episode_rewards = [0.0]  # sum of rewards for all agents
         agent_rewards = [[0.0] for _ in range(env.n)]  # individual agent reward
-        final_ep_rewards = []  # sum of rewards for training curve
-        final_ep_ag_rewards = []  # agent rewards for training curve
+
+        if arglist.restore:
+            final_ep_rewards = list(np.load(arglist.plots_dir + arglist.exp_name + '_episode_rewards.npy'))
+            final_ep_ag_rewards = list(np.load(arglist.plots_dir + arglist.exp_name + '_agent_rewards.npy'))
+            final_ep_ag_rewards = [list(a) for a in final_ep_ag_rewards]
+        else:
+            final_ep_rewards = []  # sum of rewards for training curve
+            # final_ep_ag_rewards = []  # agent rewards for training curve
+            final_ep_ag_rewards = [[0.0] for _ in range(env.n)]  # agent rewards for training curve
+
         agent_info = [[[]]]  # placeholder for benchmarking info
         saver = tf.train.Saver()
         obs_n = env.reset()
@@ -106,10 +147,13 @@ def train(arglist):
         train_step = 0
         t_start = time.time()
 
+        print("number of agents in the environment are: ", env.n)
+        episode_avg_rewards = [0.0]
+        agent_avg_rewards = [[0.0] for _ in range(env.n)]
         print('Starting iterations...')
         while True:
             # get action
-            action_n = [agent.action(obs) for agent, obs in zip(trainers,obs_n)]
+            action_n = [agent.action(obs) for agent, obs in zip(trainers, obs_n)]
             # environment step
             new_obs_n, rew_n, done_n, info_n = env.step(action_n)
             episode_step += 1
@@ -155,10 +199,34 @@ def train(arglist):
 
             # update all trainers, if not in display or benchmark mode
             loss = None
-            for agent in trainers:
-                agent.preupdate()
-            for agent in trainers:
-                loss = agent.update(trainers, train_step)
+            # for agent in trainers:
+            #     agent.preupdate()
+            # for agent in trainers:
+            #     loss = agent.update(trainers, train_step)
+
+            for m in range(0, len(trainers)):
+                agent = trainers[m]
+
+                if not arglist.train_adversaries and m > num_adversaries:
+                    agent.preupdate()
+
+                if not arglist.train_positive_agent and m <= num_adversaries:
+                    agent.preupdate()
+
+                if arglist.train_positive_agent and arglist.train_adversaries:
+                    agent.preupdate()
+
+            for m in range(0, len(trainers)):
+                agent = trainers[m]
+
+                if not arglist.train_adversaries and m > num_adversaries:
+                    loss = agent.update(trainers, train_step)
+
+                if not arglist.train_positive_agent and m <= num_adversaries:
+                    loss = agent.update(trainers, train_step)
+
+                if arglist.train_positive_agent and arglist.train_adversaries:
+                    loss = agent.update(trainers, train_step)
 
             # save model, display training output
             if terminal and (len(episode_rewards) % arglist.save_rate == 0):
@@ -166,16 +234,22 @@ def train(arglist):
                 # print statement depends on whether or not there are adversaries
                 if num_adversaries == 0:
                     print("steps: {}, episodes: {}, mean episode reward: {}, time: {}".format(
-                        train_step, len(episode_rewards), np.mean(episode_rewards[-arglist.save_rate:]), round(time.time()-t_start, 3)))
+                        train_step, len(episode_rewards), np.mean(episode_rewards[-arglist.save_rate:]),
+                        round(time.time() - t_start, 3)))
                 else:
                     print("steps: {}, episodes: {}, mean episode reward: {}, agent episode reward: {}, time: {}".format(
                         train_step, len(episode_rewards), np.mean(episode_rewards[-arglist.save_rate:]),
-                        [np.mean(rew[-arglist.save_rate:]) for rew in agent_rewards], round(time.time()-t_start, 3)))
+                        [np.mean(rew[-arglist.save_rate:]) for rew in agent_rewards], round(time.time() - t_start, 3)))
                 t_start = time.time()
                 # Keep track of final episode reward
                 final_ep_rewards.append(np.mean(episode_rewards[-arglist.save_rate:]))
-                for rew in agent_rewards:
-                    final_ep_ag_rewards.append(np.mean(rew[-arglist.save_rate:]))
+                # for rew in agent_rewards:
+                #     final_ep_ag_rewards.append(np.mean(rew[-arglist.save_rate:]))
+
+                # for rew in agent_rewards:
+                for j in range(len(agent_rewards)):
+                    rew = agent_rewards[j]
+                    final_ep_ag_rewards[j].append(np.mean(rew[-arglist.save_rate:]))
 
             # saves final episode reward for plotting training curve later
             if len(episode_rewards) > arglist.num_episodes:
@@ -186,7 +260,25 @@ def train(arglist):
                 with open(agrew_file_name, 'wb') as fp:
                     pickle.dump(final_ep_ag_rewards, fp)
                 print('...Finished total of {} episodes.'.format(len(episode_rewards)))
+
+                agent_rewards = np.array(final_ep_ag_rewards)
+                episode_rewards = np.array(final_ep_rewards)
+
+                np.save(arglist.plots_dir + arglist.exp_name + '_agent_rewards.npy', agent_rewards)
+                np.save(arglist.plots_dir + arglist.exp_name + '_episode_rewards.npy', episode_rewards)
+
+                fig, ax = plt.subplots()
+                for k in range(len(agent_rewards)):
+                    ax.plot(agent_rewards[k], label="agent_" + str(k))
+
+                ax.plot(episode_rewards, label="total")
+
+                ax.legend()
+                plt.savefig(arglist.plots_dir + arglist.exp_name + '_plot.png')
+                plt.show()
+
                 break
+
 
 if __name__ == '__main__':
     arglist = parse_args()
